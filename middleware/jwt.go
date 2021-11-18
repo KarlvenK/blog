fcd1f8143ce30fe30d8d1abedefe4d3d68299934
Karlven <karlven@outlook.com>
2021-11-18 23:46:45 +0800

    jwt(fixed method)

---
+++ middleware/jwt.go
@@ -0,0 +1,84 @@
+package middleware
+
+import (
+	"blog/utils"
+	"blog/utils/errmsg"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var JwtKey = []byte(utils.JwtKey)
+var code int
+
+type MyClaims struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	jwt.StandardClaims
+}
+
+//generate token
+func SetToken(username string, password string) (string, int) {
+	expireTime := time.Now().Add(10 * time.Hour)
+	SetClaims := MyClaims{
+		username,
+		password,
+		jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "ginblog",
+		},
+	}
+
+	reqClaims := jwt.NewWithClaims(jwt.SigningMethodES256, SetClaims)
+	token, err := reqClaims.SignedString(JwtKey)
+	if err != nil {
+		return "", errmsg.ERROR
+	}
+	return token, errmsg.SUCCESS
+}
+
+//verify toke
+func CheckToken(token string) (*MyClaims, int) {
+	settoken, _ := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
+	})
+	if key, code := settoken.Claims.(*MyClaims); code && settoken.Valid {
+		return key, errmsg.SUCCESS
+	} else {
+		return nil, errmsg.ERROR
+	}
+}
+
+//jwt middleware
+func JwtToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenHeader := c.Request.Header.Get("Authorization")
+
+		if tokenHeader == "" {
+			code = errmsg.ERROR_TOKEN_EXIST
+			//c.Abort()
+		}
+		checkToken := strings.SplitN(tokenHeader, " ", 2)
+		if len(checkToken) != 2 && checkToken[0] != "Bearer" {
+			code = errmsg.ERROR_TOKEN_TYPE_WRONG
+			c.Abort()
+		}
+		key, Tcode := CheckToken(checkToken[1])
+		if Tcode == errmsg.ERROR {
+			code = errmsg.ERROR_TOKEN_WRONG
+			c.Abort()
+		}
+		if time.Now().Unix() > key.ExpiresAt {
+			code = errmsg.ERROR_TOKEN_RUNTIME
+			c.Abort()
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"code":    code,
+			"message": errmsg.GetErrMsg(code),
+		})
+		c.Set("username", key.Username)
+		c.Next()
+	}
+}
